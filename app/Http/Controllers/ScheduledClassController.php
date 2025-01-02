76b8b70a547de130437034af58397b7a54ae091a
Gene Kelley <phpcodehead@gmail.com>
2025-01-02 14:52:59 -0600

    Add Instructor class schedule route and view for creating a ScheduledClass.

---
+++ app/Http/Controllers/ScheduledClassController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ClassType;
+use Illuminate\Http\Request;
+
+class ScheduledClassController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $classTypes = ClassType::all();
+
+        return view('instructor.schedule.create', compact('classTypes'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
