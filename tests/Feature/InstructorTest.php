f2cbc3730eec901047fa8de02fb81b417bf7cb03
Gene Kelley <phpcodehead@gmail.com>
2025-03-02 16:22:56 -0600

    Add database test.

--- tests/Feature/InstructorTest.php
+++ tests/Feature/InstructorTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\ClassType;
+use App\Models\ScheduledClass;
 use App\Models\User;
 use Database\Seeders\ClassTypeSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -35,13 +36,49 @@ class InstructorTest extends TestCase
 
         $this->seed(ClasstypeSeeder::class);
 
+        $date = date('Y-m-d', strtotime('+5 days'));
+        $time = date('H:i:s', strtotime('+5 hours'));
+
         $response = $this->actingAs($user)
             ->post('instructor/schedule', [
                 'class_type_id' => ClassType::first()->id,
-                'date' => date('Y-m-d', strtotime('+5 days')),
-                'time' => date('H:i:s', strtotime('+5 hours')),
+                'date' => $date,
+                'time' => $time,
+        ]);
+
+        $this->assertDatabaseHas('scheduled_classes', [
+            'class_type_id' => ClassType::first()->id,
+            'date_time' => $date . ' ' . $time,
         ]);
 
         $response->assertRedirectToRoute('schedule.index');
+    }
+
+    public function test_instructor_can_cancel_class(): void
+    {
+        // Given
+        $user = User::factory()->create([
+            'role' => 'instructor'
+        ]);
+
+        $this->seed(ClasstypeSeeder::class);
+
+        $date = date('Y-m-d', strtotime('+4 days'));
+        $time = date('H:i:s', strtotime('+2 hours'));
+
+        $scheduledClass = ScheduledClass::create([
+            'instructor_id' => $user->id,
+            'class_type_id' => ClassType::first()->id,
+            'date_time' => $date . ' ' . $time,
+        ]);
+
+        // When
+        $response = $this->actingAs($user)
+            ->delete('instructor/schedule/' . $scheduledClass->id);
+
+        // Then
+        $this->assertDatabaseMissing('scheduled_classes', [
+            'id' => $scheduledClass->id,
+        ]);
     }
 }
